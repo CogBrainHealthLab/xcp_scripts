7d920009834c1ecc27f0349e33a28ba7dde4f809
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2025-10-05 20:20:29 +0800

    Update cifti.py

--- cifti.py
+++ cifti.py
@@ -206,7 +206,7 @@ the following post-processing was performed.
     downcast_data = pe.Node(
         ConvertTo32(),
         name='downcast_data',
-        mem_gb=mem_gbx
+        mem_gb=mem_gbx['timeseries'],
     )
 
     workflow.connect([
@@ -300,7 +300,6 @@ the following post-processing was performed.
     qc_report_wf = init_qc_report_wf(
         TR=TR,
         head_radius=head_radius,
-        mem_gb=mem_gbx,
         name='qc_report_wf',
     )
 
